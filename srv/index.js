bea9bb39073f87e4b2b0cb5cc8ae99172875e995
Joachim <joachim.vanpraet@flexso.com>
2019-11-13 15:02:47 +0100

    update for blog

---
+++ srv/index.js
@@ -0,0 +1,41 @@
+const logging = require('@sap/logging');
+const express = require('express');
+const app = express();
+const {SapCfAxios} = require('sap-cf-axios');
+
+
+const dummy = SapCfAxios('DUMMY');
+const appContext = logging.createAppContext();
+
+app.use(logging.middleware({ appContext: appContext, logNetwork: true }));
+app.use(express.json());
+
+const handleRequest = async (req, res) => {
+    // do the dummy request
+    var logger = req.loggingContext.getLogger('/Application/Destination');
+  
+    try {
+        logger.info('Starting the request to DUMMY destination ...');
+        const response = await dummy({
+            method: 'POST',
+            url: '/test/test2',
+            data: {
+                "foo": "bar"
+            }
+        });
+        logger.info('Request done ...');
+        res.send(response.data);
+    } catch (error) {
+     
+        logger.info('Request failed ...');
+        logger.error(error);
+        res.reject(error);
+    }
+}
+
+app.all('*', handleRequest);
+
+const port = process.env.PORT || 3000;;
+app.listen(port, function () {
+  console.log('myapp listening on port ' + port);
+});
